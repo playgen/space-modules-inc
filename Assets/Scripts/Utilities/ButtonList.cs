b08bcc411832a9e56b250767b234f059612f3638
Ben Hirsh <ben@playgen.com>
2017-01-05 10:01:15 +0000

    Custom gradient script, localization added to UI text & progress on seperating button images into dynamic text.

--- Assets/Scripts/Utilities/ButtonList.cs
+++ Assets/Scripts/Utilities/ButtonList.cs
@@ -29,6 +29,49 @@ public class ButtonList
         }
     }
 
+    public void BestFit()
+    {
+        if (_buttons != null && _buttons.Length > 0)
+        {
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)_buttons[0].transform.parent);
+        }
+        int smallestFontSize = 0;
+        foreach (var textObj in _buttons)
+        {
+            var text = textObj.GetComponentInChildren<Text>();
+            if (!text)
+            {
+                continue;
+            }
+            text.resizeTextForBestFit = true;
+            text.resizeTextMinSize = 1;
+            text.resizeTextMaxSize = 100;
+            text.cachedTextGenerator.Invalidate();
+            text.cachedTextGenerator.Populate(text.text, text.GetGenerationSettings(text.rectTransform.rect.size));
+            text.resizeTextForBestFit = false;
+            var newSize = text.cachedTextGenerator.fontSizeUsedForBestFit;
+            var newSizeRescale = text.rectTransform.rect.size.x / text.cachedTextGenerator.rectExtents.size.x;
+            if (text.rectTransform.rect.size.y / text.cachedTextGenerator.rectExtents.size.y < newSizeRescale)
+            {
+                newSizeRescale = text.rectTransform.rect.size.y / text.cachedTextGenerator.rectExtents.size.y;
+            }
+            newSize = Mathf.FloorToInt(newSize * newSizeRescale);
+            if (newSize < smallestFontSize || smallestFontSize == 0)
+            {
+                smallestFontSize = newSize;
+            }
+        }
+        foreach (var textObj in _buttons)
+        {
+            var text = textObj.GetComponentInChildren<Text>();
+            if (!text)
+            {
+                continue;
+            }
+            text.fontSize = smallestFontSize;
+        }
+    }
+
     public Button GetButton(string buttonName, bool hasContainer = false)
     {
         var button = _buttons.First(o => o.name.Equals(buttonName));
