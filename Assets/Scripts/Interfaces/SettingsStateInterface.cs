d01e87db385216e388f02d4e108ff9028dc1e0d9
Ben Hirsh <ben@playgen.com>
2017-01-06 14:34:55 +0000

    Settings Styling

--- Assets/Scripts/Interfaces/SettingsStateInterface.cs
+++ Assets/Scripts/Interfaces/SettingsStateInterface.cs
@@ -7,44 +7,45 @@ using UnityEngine.UI;
 
 public class SettingsStateInterface : StateInterface
 {
-    private GameObject _settingsPanel;
-    private SettingCreation _creator;
+	private GameObject _settingsPanel;
+	private SettingCreation _creator;
+	private ButtonList _buttons;
 
-    public override void Initialize()
-    {
-        _settingsPanel = GameObjectUtilities.FindGameObject("SettingsContainer/SettingsPanelContainer");
-        _creator = _settingsPanel.GetComponentInChildren<SettingCreation>();
-        _creator.Wipe();
-        var language = _creator.Language(false);
-        var buttonLayout = _creator.HorizontalLayout("Buttons");
-        var back = _creator.Custom<Button>("Cancel", SettingObjectType.Button, true);
-        _creator.AddToLayout(buttonLayout, back);
-        back.onClick.AddListener(OnBackClick);
-        var apply = _creator.Custom<Button>("Apply", SettingObjectType.Button, true);
-        apply.onClick.AddListener(delegate { OnApplyClick(language); });
-        _creator.AddToLayout(buttonLayout, apply);
+	public override void Initialize()
+	{
+		_buttons = new ButtonList("SettingsContainer/SettingsPanelContainer/ButtonContainer");
+		var backButton = _buttons.GetButton("BackButton");
+		var applyButton = _buttons.GetButton("ApplyButton");
+		_settingsPanel = GameObjectUtilities.FindGameObject("SettingsContainer/SettingsPanelContainer/SettingsPanel");
+		_creator = _settingsPanel.GetComponentInChildren<SettingCreation>();
+		_creator.Wipe();
+		var language = _creator.Language(false);
+		applyButton.onClick.AddListener(delegate { OnApplyClick(language); });
+		backButton.onClick.AddListener(OnBackClick);
+	}
 
-    }
+	private void OnApplyClick(Dropdown language)
+	{
+		Localization.UpdateLanguage(language.value);
+		_buttons.BestFit();
+	}
 
-    private void OnApplyClick(Dropdown language)
-    {
-        Localization.UpdateLanguage(language.value);
-    }
+	private void OnBackClick()
+	{
+		EnqueueCommand(new PreviousStateCommand());
+	}
 
-    private void OnBackClick()
-    {
-        EnqueueCommand(new PreviousStateCommand());
-    }
+	public override void Enter()
+	{
+		_buttons.BestFit();
 
-    public override void Enter()
-    {
-        GameObjectUtilities.FindGameObject("SettingsContainer/SettingsPanelContainer").SetActive(true);
-        GameObjectUtilities.FindGameObject("BackgroundContainer/MenuBackgroundImage").SetActive(true);
-    }
+		GameObjectUtilities.FindGameObject("SettingsContainer/SettingsPanelContainer").SetActive(true);
+		GameObjectUtilities.FindGameObject("BackgroundContainer/MenuBackgroundImage").SetActive(true);
+	}
 
-    public override void Exit()
-    {
-        GameObjectUtilities.FindGameObject("SettingsContainer/SettingsPanelContainer").SetActive(false);
-        GameObjectUtilities.FindGameObject("BackgroundContainer/MenuBackgroundImage").SetActive(false);
-    }
+	public override void Exit()
+	{
+		GameObjectUtilities.FindGameObject("SettingsContainer/SettingsPanelContainer").SetActive(false);
+		GameObjectUtilities.FindGameObject("BackgroundContainer/MenuBackgroundImage").SetActive(false);
+	}
 }
