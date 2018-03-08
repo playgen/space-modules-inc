21fb055722a265011067ac3b10a90adc41f1b0bb
BAYMAX\Felix <felix@playgen.com>
2018-03-08 13:31:32 +0000

    Questionnaire no longer breaks the menu state when returning

--- Assets/Scripts/GameStates/Inputs/LoadingStateInput.cs
+++ Assets/Scripts/GameStates/Inputs/LoadingStateInput.cs
@@ -28,6 +28,7 @@ public class LoadingStateInput : TickStateInput
 	{
 		GameObjectUtilities.FindGameObject("SplashContainer/SplashPanelContainer").SetActive(true);
 		// Check for SUGAR login
+		
 		SUGARManager.Account.DisplayPanel(success =>
 		{
 			if (success)
@@ -39,6 +40,17 @@ public class LoadingStateInput : TickStateInput
 				Debug.LogError("Sign In Failed");
 			}
 		});
+
+		if (SUGARManager.CurrentUser != null)
+		{
+			// set username on account panel
+			var username = GameObject.Find("SUGAR/SUGAR Canvas/AccountPanel/Username/InputField").GetComponent<InputField>();
+			if (username != null)
+			{
+				username.text = SUGARManager.CurrentUser.Name;
+				username.textComponent.text = SUGARManager.CurrentUser.Name;
+			}
+		}
 	}
 
 	protected override void OnExit()
