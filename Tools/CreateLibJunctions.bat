69152edbcff8a51d2ee2d1febbdbfdee8ed3c4b8
Jared <>
2018-04-17 17:07:06 +0100

    Added mac link creation tool and removed unused audio link

--- Tools/CreateLibJunctions.bat
+++ Tools/CreateLibJunctions.bat
@@ -58,4 +58,3 @@
  mklink /j "..\Assets\Editor\ExcelToJsonConverter\bin" "..\lib\ExcelToJsonConverter"
  mklink /j "..\Assets\Plugins\IntegratedAuthoringTool" "..\lib\IntegratedAuthoringTool"
  mklink /j "..\Assets\Plugins\PlayGen Utilities" "..\lib\PlayGenUtilities"
- mklink /j "..\Assets\StreamingAssets\Scenarios\Audio" "..\Audio\ogg"
---
+++ Tools/MakeSymLinks.sh
@@ -0,0 +1,5 @@
+ln -s /Projects/space-modules-inc/lib/GameWork/ /Projects/space-modules-inc/Assets/GameWork
+ln -s /Projects/space-modules-inc/lib/SUGAR/ /Projects/space-modules-inc/Assets/SUGAR/bin
+ln -s /Projects/space-modules-inc/lib/ExcelToJsonConverter/ /Projects/space-modules-inc/Assets/Editor/ExcelToJsonConverter/bin
+ln -s /Projects/space-modules-inc/lib/IntegratedAuthoringTool/ /Projects/space-modules-inc/Assets/Plugins/IntegratedAuthoringTool
+ln -s /Projects/space-modules-inc/lib/PlayGenUtilities/ /Projects/space-modules-inc/Assets/Plugins/PlayGen\ Utilities
